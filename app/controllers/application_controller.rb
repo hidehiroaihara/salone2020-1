41f2987b0dd64201a16074675aa80a75dfea74cf
hidehiroaihara <70199100+hidehiroaihara@users.noreply.github.com>
2020-10-07 00:02:11 +0900

    devise ユーザーコントローラー、ビュー導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_cana, :last_name_cana, :birthday, :phone_number, :gender_id, :stylist_id, :blood_type_id, :job_id, :customer_text, :member_id, :customer_number, :age_id, :post_code, :prefecture_id, :address_all, :information_date, :information_text, :visit_time, :consent_id])
+  end
 end
