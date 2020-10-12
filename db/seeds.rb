11a37da5e24d29433520d62bed7c338052dee9b9
hidehiroaihara <70199100+hidehiroaihara@users.noreply.github.com>
2020-10-12 22:16:24 +0900

    seed 修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Admin.create!(
-#   name: 'salone',
-#   email: 'salone@salone.com',
-#   password: 'salone2020',
-#   admin: true
-# )
+Admin.create!(
+  name: 'salone',
+  email: 'salone@salone.com',
+  password: 'salone2020',
+  admin: true
+)
 StyleCategoryList.create([ {name: 'カット'},
                            { name: 'カラー' },
                          { name: 'パーマ'},
