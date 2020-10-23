4f557914b659f0cc60e448d62d167e3a97ae3099
hidehiroaihara <70199100+hidehiroaihara@users.noreply.github.com>
2020-10-23 22:19:53 +0900

    stylistモデル修正

--- app/models/nomination.rb
+++ app/models/nomination.rb
@@ -1,5 +1,6 @@
 class Nomination < ActiveHash::Base
   self.data = [
+    { id: 0, name: '選択なし' },
     { id: 1, name: '可能' },
     { id: 2, name: '不可' }
   ]
--- app/models/stylist_detail.rb
+++ app/models/stylist_detail.rb
@@ -5,14 +5,19 @@ class StylistDetail < ApplicationRecord
   belongs_to_active_hash :nomination
 
   belongs_to :stylist
-
-  if self.stylist.rank_id == 1
-    with_options presence: true do
-      validates :stylist_history
-      validates :nomination
-      validates :style_type
-    end
-  end
+  # def rank
+  #   stylists = Stylist.all
+  #   stylists.each do |stylist|
+  #     rank = stylist.stylist_detail.rank_id
+  #   end
+  # end 
+  # if Stylist.pluck(:rank_id) == 1
+  #   with_options presence: true do
+  #     validates :stylist_history
+  #     validates :nomination
+  #     validates :style_type
+  #   end
+  # end
   PRICE_REGEX = /\A[0-9]+\z/.freeze
   validates_format_of :nomination_price, with: PRICE_REGEX, message: 'には半角数字で入力して下さい'
 end
